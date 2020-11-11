26014e66632022877a080c57404dc052e07fa659
BMoClay <benmorgancleveland@gmail.com>
2020-11-11 14:01:10 -0500

    first

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,28 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select{|car| car.owner == self}
+  end
+
+  def mechanics
+    cars.map{|car| car.mechanic}
+  end
+
+  def self.average_number_of_cars
+    nc = Car.all.count
+    nc / CarOwner.all.count.to_f
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
+mike = CarOwner.new("mike")
+steve = CarOwner.new("steve")
+pat = CarOwner.new("pat")
+
+gil = Mechanic.new("gil", "clunker")
+gary = Mechanic.new("gary", "antique")
+mick = Mechanic.new("mick", "exotic")
+
+
+car1 = Car.new("subaru", "outback", "clunker", steve, gil)
+car2 = Car.new("chevy", "convertible", "antique", steve, gary)
+car3 = Car.new("porsche", "911", "exotic", pat, mick)
+car4 = Car.new("toyota", "corolla", "clunker", mike, mick)
+car5 = Car.new("toyota", "camry", "clunker", mike, mick)
+
+
 binding.pry
